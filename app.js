cc58b9d755c09d69735a168c7c6649795142afb7
guillegiuu <guille.giusso3@hotmail.com>
2023-01-23 05:05:17 -0300

    Primer Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//Mostrar
+function resetElement() {
+  document.getElementById("ocultar").style.display = "block"
+  document.getElementById("resetElement").style.visibility = "visible";
+}
+
+//Ocultar
+function ocultar() {
+  document.getElementById("ocultar").style.display = "none";
+  
+}
+
+
